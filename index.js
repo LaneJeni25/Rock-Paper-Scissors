6650b2fa2f125cefcde7ee8ad7dde5bfbcb79e91
LaneJeni25 <jenilane25@gmail.com>
2020-11-20 12:51:47 -0800

    attempt at testing 2 - bug with SSL not fixed, added start.js since it broke main

--- index.js
+++ index.js
@@ -163,4 +163,10 @@ app.get('/leaderboard', (req, res) => {
 });
 
 
+// Needed to deal with DEPTH_ZERO_SELF_SIGNED_CERT error with Heroku
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+app.listen(process.env.PORT || 5000, () => {
+    console.log("Server up and running on port: " + (process.env.PORT || 5000));
+});
+
 module.exports = app;
