ea62fdfa94807352c44be8a94e2aabc057b1d7d4
LaneJeni25 <jenilane25@gmail.com>
2020-11-20 00:33:56 -0800

    fixed upsert sql strings, updated comments, removed unnecessary POST code, but still have bug with leaderboard updating correctly

--- index.js
+++ index.js
@@ -3,14 +3,11 @@
  * Carina Take-Home Interview
  * 11/18/2020
  */
-
 const express = require('express'); // Import express.js
 const Joi = require('joi'); // Import Joi
 
 const app = express(); // Creates a new instance of an express application
-
-const bodyParser = require("body-parser");
-app.use(bodyParser.json()); // Allows for parsing of the body of POST requests, that are encoded in JSON
+app.use(express.json()); // Uses the json file
 
 /*
  * Current choices for Rock, Paper, Scissors
@@ -24,22 +21,11 @@ const choices = [
 
 const db = require('./utilities/sqlconn.js'); // Heroku database variable endpoint
 
-let middleware = require('./utilities/middleware'); // Middleware endpoint
-
 
 // Default Homepage Message
 app.get('/', (req, res) => {
    res.send('Welcome to the Carina Care Rock-Paper-Scissors Take-Home Interview!'
    + '\nPlease choose rock, paper, or scissors and enter your player-name to start.');
-
-
-   // HTML version of homepage
-   /*
-   res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('<h' + i + ' style="color:black">Welcome to the Carina Care Rock-Paper-Scissors Take-Home Interview!
-    Please choose rock, paper, or scissors and enter your player-name to start.</h' + i + '>');
-    res.end(); //end the response
-    */
 });
 
 /*
@@ -52,6 +38,7 @@ app.get('/shoot', (req, res) => {
     let playerName = req.query['player_name'];
 
     // Check if valid information was given for play and player_name
+
     const { error } = validatePlay(req.query);
     if (error) {
         res.status(400).send(error.details[0].message)
@@ -59,10 +46,12 @@ app.get('/shoot', (req, res) => {
     }
 
     // Computer chooses play at random
+
     let randomNum = Math.floor(Math.random() * choices.length);
     let choice = choices[randomNum];
 
     // Compare player versus computer for win/loss/tie and update leaderboard
+
     if (play === choice) { // player ties
         updateLeaderboard(playerName, 0, res);
     } else {
@@ -90,27 +79,6 @@ app.get('/shoot', (req, res) => {
 });
 
 /*
- * Handles GET endpoint at /leaderboard that returns a scoreboard of
- * each player's name and the number of rounds they have won
- * in descending order in the form an array of objects in
- * a JSON body.
- */
-app.get('/leaderboard', (req, res) => {
-    db.manyOrNone('SELECT * FROM leaderboard')
-        .then((data) => {
-            res.send({
-                leaderboard: data
-            });
-        }).catch((err) => {
-            res.send({
-                success: false,
-                error: err + " - Could not access leaderboard data"
-            })
-        });
-});
-
-
-/*
  * Validates if player entered a valid choice of rock, paper, or scissors
  * and entered a name for themself.
  */
@@ -128,7 +96,7 @@ function validatePlay(data) {
 }
 
 /*
- * Given the corresponding value:
+ * Given the corresponding value, player name and response:
  * -1 = loss
  * 0 = tie
  * 1 = win
@@ -139,39 +107,60 @@ function validatePlay(data) {
 function updateLeaderboard(playerName, outcome, res) {
     if (outcome === -1) {
         db.none("INSERT INTO leaderboard(player_name, wins, losses, ties) " +
-            "VALUES($1, 0, 1, 0) ON CONFLICT(player_name) DO UPDATE SET losses=excluded.losses+1",[playerName])
+            "VALUES($1, 0, 1, 0) ON CONFLICT(player_name) DO UPDATE SET losses=leaderboard.losses+1",[playerName])
             .then(() => {
                 res.status(200).send(playerName + ' loses the round');
             }).catch((err) => {
             res.send({
                 success: false,
-                error: err + " - Could not update leaderboard"
+                error: err + " - Could not update leaderboard for loss"
             });
         });
     } else if (outcome === 0) {
         db.none("INSERT INTO leaderboard(player_name, wins, losses, ties) " +
-            "VALUES($1, 0, 0, 1) ON CONFLICT(player_name) DO UPDATE SET ties=excluded.ties+1",[playerName])
+            "VALUES($1, 0, 0, 1) ON CONFLICT(player_name) DO UPDATE SET ties=leaderboard.ties+1",[playerName])
             .then(() => {
                 res.status(200).send(playerName + ' ties the round');
             }).catch((err) => {
             res.send({
                 success: false,
-                error: err + " - Could not update leaderboard"
+                error: err + " - Could not update leaderboard for tie"
             });
         });
     } else {
         db.none("INSERT INTO leaderboard(player_name, wins, losses, ties) " +
-            "VALUES($1, 1, 0, 0) ON CONFLICT(player_name) DO UPDATE SET wins=excluded.wins+1",[playerName])
+            "VALUES($1, 1, 0, 0) ON CONFLICT(player_name) DO UPDATE SET wins=leaderboard.wins+1",[playerName])
             .then(() => {
                 res.status(200).send(playerName + ' wins the round');
             }).catch((err) => {
             res.send({
                 success: false,
-                error: err + " - Could not update leaderboard"
+                error: err + " - Could not update leaderboard for win"
             });
         });
     }
 }
+
+
+/*
+ * Handles GET endpoint at /leaderboard that returns a scoreboard of
+ * each player's name and the number of rounds they have won
+ * in descending order in the form an array of objects in
+ * a JSON body.
+ */
+app.get('/leaderboard', (req, res) => {
+    db.manyOrNone('SELECT * FROM leaderboard ORDER BY wins DESC')
+        .then((data) => {
+            res.send({
+                leaderboard: data
+            });
+        }).catch((err) => {
+        res.send({
+            success: false,
+            error: err + " - Could not access leaderboard data"
+        })
+    });
+});
 
 
 // Needed to deal with DEPTH_ZERO_SELF_SIGNED_CERT error with Heroku
